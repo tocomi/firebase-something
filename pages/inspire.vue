a67b16383c9063b0c53f61aac1a38bd61d70932f
tocomi <tocomi@wish.ocn.ne.jp>
2019-07-13 01:33:25 +0900

    show api data

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -16,6 +16,14 @@
       </blockquote>
       <v-btn @click="smash">SMAAAAAAASH!!</v-btn>
       <p>{{ count }}</p>
+      <li v-for="item in items" :key="item.id">
+        <h4>
+          <span>{{ item.title }}</span>
+          <small> by {{ item.user.id }}</small>
+        </h4>
+        <div>{{ item.body.slice(0, 130) }}......</div>
+        <p><a :href="item.url" target="_blank">{{ item.url }}</a></p>
+      </li>
     </v-flex>
   </v-layout>
 </template>
@@ -26,7 +34,6 @@ import { Component, Vue } from 'vue-property-decorator'
 @Component({
   async asyncData({ app }) {
     const items = await app.$axios.$get('https://qiita.com/api/v2/items?query=tag:nuxt.js')
-    console.log(items)
     return {
       items
     }
