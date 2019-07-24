9a10bb4f62a0b98f98d6eb54d0bd435723cb03ec
tocomi <tocomi@wish.ocn.ne.jp>
2019-07-25 01:54:29 +0900

    add swallows page

---
+++ pages/swallows.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  v-layout
+    v-flex(text-xs-center)
+      | hoge
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Inspire extends Vue {
+
+  count: number = 1
+
+  smash(): void {
+    this.count *= 2
+  }
+
+}
+</script>
