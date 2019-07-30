8aad12f95ca09cacd66366082a9e55e50b6f4276
tocomi <tocomi@wish.ocn.ne.jp>
2019-07-30 23:02:16 +0900

    make user detail page

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -19,7 +19,12 @@
       <li v-for="item in items" :key="item.id">
         <h4>
           <span>{{ item.title }}</span>
-          <small> by {{ item.user.id }}</small>
+          <small>
+            <span>by </span>
+            <nuxt-link :to="`/users/${item.user.id}`">
+              {{ item.user.id }}
+            </nuxt-link>
+          </small>
         </h4>
         <div>{{ item.body.slice(0, 130) }}......</div>
         <p><a :href="item.url" target="_blank">{{ item.url }}</a></p>
--- pages/users/_id.vue
+++ pages/users/_id.vue
@@ -1,26 +1,40 @@
-<template>
-  <v-layout>
-    <v-flex text-xs-center>
-      <img
-        src="/v.png"
-        alt="Vuetify.js"
-        class="mb-5"
-      >
-      <blockquote class="blockquote">
-        <p>{{ userId }}</p>
-      </blockquote>
-    </v-flex>
-  </v-layout>
+<template lang="pug">
+  v-layout
+    v-flex(text-xs-center)
+      img.mb-5(src="/v.png" alt="Vuetify.js")
+      blockquote.blockquote
+        h3 {{ userId }}
+        img(:src="user.profile_image_url" width="120" alt="")
+        p {{ user.description || 'No description' }}
+        p
+          nuxt-link(to="/")
+            small
+              b Top page
+        h3 {{ user.id }}さんの投稿一覧
+        ul
+          li(v-for="item in items" :key="item.id")
+            h4
+              span {{ item.title }}
+            div {{ item.body.slice(0, 130) }}...
+            p
+              a(target="_black" :href="item.url") {{ item.url }}
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import Component from 'vue-class-component'
 
-@Component
+@Component({
+  async asyncData({ route, app }) {
+    const userId: string = route.params.id
+    const user: object = await app.$axios.$get(`https://qiita.com/api/v2/users/${userId}`)
+    const items: object = await app.$axios.$get(`https://qiita.com/api/v2/items?query=user:${userId}`)
+    return {
+      user, items,
+    }
+  }
+})
 export default class Id extends Vue {
-
   userId: string = this.$nuxt.$route.params.id
-
 }
 </script>
