2b7bbb0d06a84f0b6d10a400e59ae5348a67f21e
tocomi <tocomi@wish.ocn.ne.jp>
2019-07-31 00:05:40 +0900

    make user detail page

--- pages/users/_id.vue
+++ pages/users/_id.vue
@@ -1,7 +1,6 @@
 <template lang="pug">
   v-layout
     v-flex(text-xs-center)
-      img.mb-5(src="/v.png" alt="Vuetify.js")
       blockquote.blockquote
         h3 {{ userId }}
         img(:src="user.profile_image_url" width="120" alt="")
