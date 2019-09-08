823e2b8154b1c922c79852ca0c8524cae0522549
tocomi <tocomi@wish.ocn.ne.jp>
2019-09-08 14:52:20 +0900

    add firebase plugin

---
+++ plugins/firebase.js
@@ -0,0 +1,9 @@
+import firebase from 'firebase'
+import 'firebase/firestore'
+const firebaseKey = require('../firebaseKey.json')
+
+firebase.initializeApp(...firebaseKey)
+const db = firebase.firestore
+const setting = { timestampsInSnapshots: true }
+db.setting(setting)
+return { db }
