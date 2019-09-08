1b1eb28f14331df9c1c219c053716eb32e974bf5
tocomi <tocomi@wish.ocn.ne.jp>
2019-09-08 18:28:31 +0900

    resolve error

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -2,8 +2,6 @@ import firebase from 'firebase'
 import 'firebase/firestore'
 const firebaseKey = require('../firebaseKey.json')
 
-firebase.initializeApp(...firebaseKey)
-const db = firebase.firestore
-const setting = { timestampsInSnapshots: true }
-db.setting(setting)
+firebase.initializeApp({ ...firebaseKey })
+const db = firebase.firestore()
 export { db }
